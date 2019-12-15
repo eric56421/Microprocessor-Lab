27e830dd3f672a9011e35e8e56f2fb10be4162d9
Fishbone Huang <eric56421@gmail.com>
2019-12-16 03:17:57 +0800

    Lab08 codes with comments

--- lab08/lab08_1.asm
+++ lab08/lab08_1.asm
@@ -1,10 +1,14 @@
+; show student ID on LCD with Label
+; P1 : Data port
+; P2 : E, RW, RS
+
         ORG 00H
         JMP SETUP
 
         ORG 30H
 SETUP:
-        MOV P1, 00H
-        MOV P2, 00H
+        MOV P1, 00H             ; set P1 as output
+        MOV P2, 00H             ; set P2 as output
         MOV A, #00111000B       ; LEN:8, LINES:2, FONT:7
         CALL CMDWRT
         MOV A, #00001110B       ; DISPLAY:ON, CURSOR_UNDERLINE:ON, BLINKING:OFF
@@ -15,44 +19,44 @@ SETUP:
         CALL CMDWRT
 
 MAIN:
-        CALL SHOW_ID
-        CALL SHOW_LABEL
+        CALL SHOW_ID            ; show student ID on first row
+        CALL SHOW_LABEL         ; show label on second row
 HERE:   JMP HERE
 
 SHOW_ID:
         MOV A, #10000000B       ; SET CURSOR PLACE(0->), ROW0, COL0
-        CALL CMDWRT
-        MOV R0, #00H
-        MOV DPTR, #TABLE_ID
-SHOW_ID_LOOP:
-        MOV A, R0
-        MOVC A, @A+DPTR
-        CALL DATAWRT
-        INC R0
-        CJNE R0, #07H, SHOW_ID_LOOP
+        CALL CMDWRT             
+        MOV R0, #00H            ; R0 is the index of table
+        MOV DPTR, #TABLE_ID     ; DPTR point to ID table
+SHOW_ID_LOOP:                   
+        MOV A, R0               ; ---V
+        MOVC A, @A+DPTR         ; get data from table
+        CALL DATAWRT            ; ---A
+        INC R0                  ; index++
+        CJNE R0, #07H, SHOW_ID_LOOP     ; to show 7 characters
         RET
 
 SHOW_LABEL:
         MOV A, #11000000B       ; SET CURSOR PLACE(0->), ROW1, COL0
-        CALL CMDWRT
-        MOV R0, #00H
-        MOV DPTR, #TABLE_LABEL
-SHOW_LABEL_LOOP:
-        MOV A, R0
-        MOVC A, @A+DPTR
-        CALL DATAWRT
-        INC R0
-        CJNE R0, #10H, SHOW_LABEL_LOOP
+        CALL CMDWRT             
+        MOV R0, #00H            ; R0 is the index of table
+        MOV DPTR, #TABLE_LABEL  ; DPTR point to LABEL table
+SHOW_LABEL_LOOP:                   
+        MOV A, R0               ; ---V
+        MOVC A, @A+DPTR         ; get data from table
+        CALL DATAWRT            ; ---A
+        INC R0                  ; index++
+        CJNE R0, #10H, SHOW_LABEL_LOOP  ; to show 16 characters
         RET
 
-CMDWRT:
+CMDWRT:                         ; Command write
         MOV P1, A
         MOV P2, #00000100B      ; 04H, E=1, RW=0, RS=0
         MOV P2, #00000000B      ; 00H, E=0, RW=0, RS=0
         CALL DELAY3MS
         RET
 
-DATAWRT:
+DATAWRT:                        ; ShowData write
         MOV P1, A
         MOV P2, #00000101B      ; 05H, E=1, RW=0, RS=1
         MOV P2, #00000001B      ; 01H, E=1, RW=0, RS=1
@@ -60,16 +64,16 @@ DATAWRT:
         RET
 
 DELAY3MS:                  
-        MOV R5, #01H    ; 1 machine cycle
+        MOV R5, #01H            ; 1 machine cycle
 DELAY3MS1:
-        MOV R6, #10H    ; 1 machine cycle
+        MOV R6, #10H            ; 1 machine cycle
 DELAY3MS2:
-        MOV R7, #0FFH   ; 1 machine cycle
+        MOV R7, #0FFH           ; 1 machine cycle
 DELAY3MS3:
-        DJNZ R7, DELAY3MS3 ; 2 machine cycle
-        DJNZ R6, DELAY3MS2 ; 2 machine cycle
-        DJNZ R5, DELAY3MS1 ; 2 machine cycle
-        RET             ; 2 machine cycle
+        DJNZ R7, DELAY3MS3      ; 2 machine cycle
+        DJNZ R6, DELAY3MS2      ; 2 machine cycle
+        DJNZ R5, DELAY3MS1      ; 2 machine cycle
+        RET                     ; 2 machine cycle
 
 TABLE_ID:
         DB "0710758"
--- lab08/lab08_2.asm
+++ lab08/lab08_2.asm
@@ -1,4 +1,7 @@
-        
+; show animation on LCD
+; P1 : Data port
+; P2 : E, RW, RS
+
         SET_CUR_POS EQU 10000111B
 
         ORG 00H
@@ -6,8 +9,8 @@
 
         ORG 30H
 SETUP:
-        MOV P1, 00H
-        MOV P2, 00H
+        MOV P1, 00H             ; set P1 as output
+        MOV P2, 00H             ; set P2 as output
         MOV A, #00111000B       ; LEN:8, LINES:2, FONT:7
         CALL CMDWRT
         MOV A, #00001100B       ; DISPLAY:ON, CURSOR_UNDERLINE:OFF, BLINKING:OFF
@@ -16,54 +19,54 @@ SETUP:
         CALL CMDWRT
         MOV A, #00000110B       ; ADD TO RIGHT, DISPLAY NOT MOVING
         CALL CMDWRT
-        CALL SETUP_ANI
+        CALL SETUP_ANI          ; to load custom characters
 
 MAIN:
         CALL SHOW_ANI
         JMP MAIN
 
-SETUP_ANI:
-        MOV DPTR, #TABLE_ANI
-        MOV R1, #40H            ; FIRST CUSTOM CHAR
-        MOV A, R1
+SETUP_ANI:                      ; To load custom chars (animation)
+        MOV DPTR, #TABLE_ANI    ; DPTR point to animation table
+        MOV R1, #40H            ; ADDRESS OF FIRST CUSTOM CHAR
+        MOV A, R1               ; to set where to store custom char
         CALL CMDWRT
-        MOV R0, #00H
+        MOV R0, #00H            ; R0 is the index of table
 SETUP_ANI_LOOP:
-        MOV A, R0
-        MOVC A, @A+DPTR
-        CALL DATAWRT
-        INC R0
-        CJNE R0, #30H, SETUP_ANI_LOOP
+        MOV A, R0               ; ---V
+        MOVC A, @A+DPTR         ; get data from table
+        CALL DATAWRT            ; ---A
+        INC R0                  ; index++
+        CJNE R0, #30H, SETUP_ANI_LOOP   ; to load 48 frames
         RET
 
-SHOW_ANI:
-        MOV R0, #00H
-SHOW_ANI_LOOP_UP:
-        MOV A, #SET_CUR_POS
+SHOW_ANI:                       ; Show a whole set of animation
+        MOV R0, #00H            ; R0 is the index (address) of CGRAM in LCD
+SHOW_ANI_LOOP_UP:               ; Loop of forward animation
+        MOV A, #SET_CUR_POS     ; to fix the position of animation
         CALL CMDWRT
-        MOV A, R0
+        MOV A, R0               ; show the char (frame) stored in that address
         CALL DATAWRT
-        CALL DELAY
-        INC R0
-        CJNE R0, #06H, SHOW_ANI_LOOP_UP
-SHOW_ANI_LOOP_DOWN:
-        DEC R0
-        MOV A, #SET_CUR_POS
+        CALL DELAY              ; stop for a while
+        INC R0                  ; index++
+        CJNE R0, #06H, SHOW_ANI_LOOP_UP         ; to show forward part
+SHOW_ANI_LOOP_DOWN:             ; Loop of backward animation
+        DEC R0                  ; index--
+        MOV A, #SET_CUR_POS     ; to fix the position of animation
         CALL CMDWRT
-        MOV A, R0
+        MOV A, R0               ; show the char (frame) stored in that address
         CALL DATAWRT
-        CALL DELAY
-        CJNE R0, #00H, SHOW_ANI_LOOP_DOWN
+        CALL DELAY              ; stop for a while
+        CJNE R0, #00H, SHOW_ANI_LOOP_DOWN       ; to show backward part
         RET
 
-CMDWRT:
+CMDWRT:                         ; Command write
         MOV P1, A
         MOV P2, #00000100B      ; 04H, E=1, RW=0, RS=0
         MOV P2, #00000000B      ; 00H, E=0, RW=0, RS=0
         CALL DELAY3MS
         RET
 
-DATAWRT:
+DATAWRT:                        ; ShowData write
         MOV P1, A
         MOV P2, #00000101B      ; 05H, E=1, RW=0, RS=1
         MOV P2, #00000001B      ; 01H, E=1, RW=0, RS=1
@@ -71,28 +74,28 @@ DATAWRT:
         RET
 
 DELAY3MS:                  
-        MOV R5, #01H    ; 1 machine cycle
+        MOV R5, #01H            ; 1 machine cycle
 DELAY3MS1:
-        MOV R6, #10H    ; 1 machine cycle
+        MOV R6, #10H            ; 1 machine cycle
 DELAY3MS2:
-        MOV R7, #0FFH   ; 1 machine cycle
+        MOV R7, #0FFH           ; 1 machine cycle
 DELAY3MS3:
-        DJNZ R7, DELAY3MS3 ; 2 machine cycle
-        DJNZ R6, DELAY3MS2 ; 2 machine cycle
-        DJNZ R5, DELAY3MS1 ; 2 machine cycle
-        RET             ; 2 machine cycle
+        DJNZ R7, DELAY3MS3      ; 2 machine cycle
+        DJNZ R6, DELAY3MS2      ; 2 machine cycle
+        DJNZ R5, DELAY3MS1      ; 2 machine cycle
+        RET                     ; 2 machine cycle
 
 DELAY:                  
-        MOV R5, #06H    ; 1 machine cycle
+        MOV R5, #06H            ; 1 machine cycle
 DELAY1:
-        MOV R6, #0FFH    ; 1 machine cycle
+        MOV R6, #0FFH           ; 1 machine cycle
 DELAY2:
-        MOV R7, #0FFH   ; 1 machine cycle
+        MOV R7, #0FFH           ; 1 machine cycle
 DELAY3:
-        DJNZ R7, DELAY3 ; 2 machine cycle
-        DJNZ R6, DELAY2 ; 2 machine cycle
-        DJNZ R5, DELAY1 ; 2 machine cycle
-        RET             ; 2 machine cycle
+        DJNZ R7, DELAY3         ; 2 machine cycle
+        DJNZ R6, DELAY2         ; 2 machine cycle
+        DJNZ R5, DELAY1         ; 2 machine cycle
+        RET                     ; 2 machine cycle
 
 TABLE_ANI:
         DB 14H, 14H, 1FH, 05H, 06H, 0AH, 0AH, 11H
